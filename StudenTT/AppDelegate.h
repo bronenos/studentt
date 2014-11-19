f432e33b3887547d82e509d460c8adfa0fe3ae98
Stan Potemkin <bronenos@gmail.com>
2014-11-19 20:12:56 +0300

    Initial Commit

---
+++ StudenTT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudenTT
+//
+//  Created by Stan Potemkin on 11/19/14.
+//  Copyright (c) 2014 bronenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudenTT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudenTT
+//
+//  Created by Stan Potemkin on 11/19/14.
+//  Copyright (c) 2014 bronenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
